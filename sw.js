ab65b1a3963f9cbaccdfa4cfb52cca84f03fe953
Jason McKenzie <email@jasonmckenzie.ca>
2026-07-07 01:02:44 -0400

    UI restructure + polish: modeless All Songs, editor on Next Up, fade-out copy, card pop, fixed tiles, Done above bench, visible build id

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
 // (matches the documented update flow: "re-open the app on Wi-Fi once to
 // pick up the new version").
 
-const CACHE_NAME = 'kickball-05410796f4';
+const CACHE_NAME = 'kickball-27dfa03afb';
 const PRECACHE_URLS = ['./', './index.html'];
 
 self.addEventListener('install', (event) => {
