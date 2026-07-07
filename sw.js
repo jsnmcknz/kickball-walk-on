2f5af32f9779c4aeb49e3ad453fdf4b1b1b960e0
Jason McKenzie <email@jasonmckenzie.ca>
2026-07-07 01:25:42 -0400

    UI polish across all pages - vertical spacing fixes

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
 // (matches the documented update flow: "re-open the app on Wi-Fi once to
 // pick up the new version").
 
-const CACHE_NAME = 'kickball-041ab08c1c';
+const CACHE_NAME = 'kickball-53470cfbe0';
 const PRECACHE_URLS = ['./', './index.html'];
 
 self.addEventListener('install', (event) => {
