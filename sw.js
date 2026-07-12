fccf0bd0ece9a5dbf2e5c62a971345fde2e68614
Jason McKenzie <email@jasonmckenzie.ca>
2026-07-12 12:44:46 -0400

    Flip scoring.enabled: true for on-device testing

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
 // (matches the documented update flow: "re-open the app on Wi-Fi once to
 // pick up the new version").
 
-const CACHE_NAME = 'kickball-7bec08ad97';
+const CACHE_NAME = 'kickball-042586b31e';
 const PRECACHE_URLS = ['./', './index.html'];
 
 self.addEventListener('install', (event) => {
