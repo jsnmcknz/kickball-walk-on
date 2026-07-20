f2e08901b40a539f4ef6a69b301924b322bfb6cd
Jason McKenzie <email@jasonmckenzie.ca>
2026-07-20 18:06:12 -0400

    Autoplay 2500ms; drop 'runs' from mercy stepper label

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
 // (matches the documented update flow: "re-open the app on Wi-Fi once to
 // pick up the new version").
 
-const CACHE_NAME = 'kickball-cb172f4f4c';
+const CACHE_NAME = 'kickball-a08c38c000';
 const PRECACHE_URLS = ['./', './index.html'];
 
 self.addEventListener('install', (event) => {
